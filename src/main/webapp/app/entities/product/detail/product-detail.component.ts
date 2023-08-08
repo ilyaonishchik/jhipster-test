69f67a3444e99602ecf28ad1a39824657aa3cf4f
Ilya Onishchik <ilyaonishchik@gmail.com>
2023-08-08 11:29:28 +0300

    add product component and template logic

--- src/main/webapp/app/entities/product/detail/product-detail.component.ts
+++ src/main/webapp/app/entities/product/detail/product-detail.component.ts
@@ -1,20 +1,28 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute, RouterModule } from '@angular/router';
 
 import SharedModule from 'app/shared/shared.module';
 import { DurationPipe, FormatMediumDatetimePipe, FormatMediumDatePipe } from 'app/shared/date';
 import { IProduct } from '../product.model';
+import { EntityService } from 'app/entities/entity.service';
+import { EntityField } from 'app/entities/entity-field';
 
 @Component({
   standalone: true,
   selector: 'jhi-product-detail',
   templateUrl: './product-detail.component.html',
   imports: [SharedModule, RouterModule, DurationPipe, FormatMediumDatetimePipe, FormatMediumDatePipe],
+  providers: [EntityService],
 })
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnInit {
   @Input() product: IProduct | null = null;
+  fields: EntityField<IProduct>[] = [];
 
-  constructor(protected activatedRoute: ActivatedRoute) {}
+  constructor(protected activatedRoute: ActivatedRoute, private entityService: EntityService) {}
+
+  ngOnInit(): void {
+    this.entityService.getConfig<IProduct>('Product').subscribe(config => (this.fields = config.fields));
+  }
 
   previousState(): void {
     window.history.back();
