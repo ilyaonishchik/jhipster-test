7d73986e63523584d197704579f02bac2f0f15c3
Ilya Onishchik <ilyaonishchik@gmail.com>
2023-08-08 11:21:20 +0300

    add customer component and template logic

--- src/main/webapp/app/entities/customer/detail/customer-detail.component.ts
+++ src/main/webapp/app/entities/customer/detail/customer-detail.component.ts
@@ -1,20 +1,28 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute, RouterModule } from '@angular/router';
 
 import SharedModule from 'app/shared/shared.module';
 import { DurationPipe, FormatMediumDatetimePipe, FormatMediumDatePipe } from 'app/shared/date';
 import { ICustomer } from '../customer.model';
+import { EntityService } from 'app/entities/entity.service';
+import { EntityField } from 'app/entities/entity-field';
 
 @Component({
   standalone: true,
   selector: 'jhi-customer-detail',
   templateUrl: './customer-detail.component.html',
   imports: [SharedModule, RouterModule, DurationPipe, FormatMediumDatetimePipe, FormatMediumDatePipe],
+  providers: [EntityService],
 })
-export class CustomerDetailComponent {
+export class CustomerDetailComponent implements OnInit {
   @Input() customer: ICustomer | null = null;
+  fields: EntityField<ICustomer>[] = [];
 
-  constructor(protected activatedRoute: ActivatedRoute) {}
+  constructor(protected activatedRoute: ActivatedRoute, private entityService: EntityService) {}
+
+  ngOnInit(): void {
+    this.entityService.getConfig<ICustomer>('Customer').subscribe(config => (this.fields = config.fields));
+  }
 
   previousState(): void {
     window.history.back();
