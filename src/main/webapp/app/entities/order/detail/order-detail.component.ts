70fbaa59981b76780ade96e56dfe1a9b0c8f9a7c
Ilya Onishchik <ilyaonishchik@gmail.com>
2023-08-08 11:26:56 +0300

    add order component and template logic

--- src/main/webapp/app/entities/order/detail/order-detail.component.ts
+++ src/main/webapp/app/entities/order/detail/order-detail.component.ts
@@ -1,20 +1,28 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute, RouterModule } from '@angular/router';
 
 import SharedModule from 'app/shared/shared.module';
 import { DurationPipe, FormatMediumDatetimePipe, FormatMediumDatePipe } from 'app/shared/date';
 import { IOrder } from '../order.model';
+import { EntityField } from 'app/entities/entity-field';
+import { EntityService } from 'app/entities/entity.service';
 
 @Component({
   standalone: true,
   selector: 'jhi-order-detail',
   templateUrl: './order-detail.component.html',
   imports: [SharedModule, RouterModule, DurationPipe, FormatMediumDatetimePipe, FormatMediumDatePipe],
+  providers: [EntityService],
 })
-export class OrderDetailComponent {
+export class OrderDetailComponent implements OnInit {
   @Input() order: IOrder | null = null;
+  fields: EntityField<IOrder>[] = [];
 
-  constructor(protected activatedRoute: ActivatedRoute) {}
+  constructor(protected activatedRoute: ActivatedRoute, private entityService: EntityService) {}
+
+  ngOnInit(): void {
+    this.entityService.getConfig<IOrder>('Order').subscribe(config => (this.fields = config.fields));
+  }
 
   previousState(): void {
     window.history.back();
